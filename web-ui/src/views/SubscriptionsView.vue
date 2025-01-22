1d4b2c2194b196551a09b0a421dead848e1f3bf2
Brian099 <luohuafengchuiqi@163.com>
2025-01-22 12:23:26 +0800

    屏蔽远程版本号

--- web-ui/src/views/SubscriptionsView.vue
+++ web-ui/src/views/SubscriptionsView.vue
@@ -65,14 +65,14 @@
     </el-row>
     <el-row>
       PG包本地： {{ pgLocal }}
-      PG包远程： {{ pgRemote }}
+      
       <span class="hint"></span>
       <span v-if="pgLocal==pgRemote"><el-icon color="green"><Check /></el-icon></span>
       <span v-else><el-icon color="orange"><Warning /></el-icon></span>
     </el-row>
     <el-row>
       真心包本地： {{ zxLocal }}
-      真心包远程： {{ zxRemote }}
+      
       <span class="hint"></span>
       <span v-if="zxLocal==zxRemote"><el-icon color="green"><Check /></el-icon></span>
       <span v-else><el-icon color="orange"><Warning /></el-icon></span>
@@ -392,32 +392,8 @@ const load = () => {
 const loadVersion = () => {
   axios.get("/pg/version").then(({data}) => {
     pgLocal.value = data.local
+    pgRemote.value = data.remote
   })
-  fetch("http://www.fish2018.us.kg/p/jsm.json")
-  .then(response => response.json())
-  .then(data => {
-    // 提取非空的zip字段
-    const zipArray = data.sites.filter(site => site.zip).map(site => site.zip);
-
-    // 提取版本号
-    const remoteVersions = zipArray.map(zip => {
-      const match = zip.match(/pg\.(.*)\.zip/);
-      return match ? match[1] : null;
-    }).filter(version => version !== null);
-
-    // 假设我们使用第一个提取的版本号
-    const REMOTE = remoteVersions[0];
-
-    // 设置pgRemote的值
-    if (REMOTE) {
-      pgRemote.value = REMOTE;
-    } else {
-      console.error("No valid PG version found in zip files.");
-    }
-  })
-  .catch(error => {
-    console.error("Failed to fetch or parse JSON data:", error);
-  });
   axios.get("/zx/version").then(({data}) => {
     zxLocal.value = data.local
     zxRemote.value = data.remote
